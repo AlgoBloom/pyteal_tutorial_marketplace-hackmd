fee185f5d98aacf534b65b4f10891bda651f46c1
Joshua Bloom <bloomblockchain@gmail.com>
2022-09-17 19:12:15 -0400

    Corrected errors in marketplace pyteal contract.

--- marketplace_contract.py
+++ marketplace_contract.py
@@ -14,7 +14,7 @@ class Product:
     def application_creation(self):     # creates a method for the Product class
         return Seq([
             Assert(Txn.application_args.length() == Int(4)),
-            Assert(Txn.note() == Bytes("tutorial0-marketplace:uv1")),
+            Assert(Txn.note() == Bytes("tutorial-marketplace:uv1")),
             Assert(Btoi(Txn.application_args[3]) > Int(0)),
             App.globalPut(self.Variables.name, Txn.application_args[0]),
             App.globalPut(self.Variables.image, Txn.application_args[1]),
