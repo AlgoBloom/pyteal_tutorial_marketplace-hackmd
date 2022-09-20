2f205068dd2eda9456b069072479dcdbb9982894
Joshua Bloom <bloomblockchain@gmail.com>
2022-09-19 20:09:00 -0400

    Lastly, we are going to add the functionality of the marketplace to our app. We try to connect to the wallet, and if we are connected, we fetch the products. Then we display the products in the console, making use of the getProductsActions function that we just created. If we are not connected, we display a button that we will connect to the wallet by calling the connectWallet function defined above.

--- marketplace-tutorial-react/src/utils/marketplace.js
+++ marketplace-tutorial-react/src/utils/marketplace.js
@@ -25,69 +25,6 @@ class Product {
         this.appId = appId;
         this.owner = owner;
     }
-}// Compile smart contract in .teal format to program
-const compileProgram = async (programSource) => {
-    let encoder = new TextEncoder();
-    let programBytes = encoder.encode(programSource);
-    let compileResponse = await algodClient.compile(programBytes).do();
-    return new Uint8Array(Buffer.from(compileResponse.result, "base64"));
-}
-
-// CREATE PRODUCT: ApplicationCreateTxn
-export const createProductAction = async (senderAddress, product) => {
-    console.log("Adding product...")
-
-    let params = await algodClient.getTransactionParams().do();
-    params.fee = algosdk.ALGORAND_MIN_TX_FEE;
-    params.flatFee = true;
-
-    // Compile programs
-    const compiledApprovalProgram = await compileProgram(approvalProgram)
-    const compiledClearProgram = await compileProgram(clearProgram)
-
-    // Build note to identify transaction later and required app args as Uint8Arrays
-    let note = new TextEncoder().encode(marketplaceNote);
-    let name = new TextEncoder().encode(product.name);
-    let image = new TextEncoder().encode(product.image);
-    let description = new TextEncoder().encode(product.description);
-    let price = algosdk.encodeUint64(product.price);
-
-    let appArgs = [name, image, description, price]
-
-    // Create ApplicationCreateTxn
-    let txn = algosdk.makeApplicationCreateTxnFromObject({
-        from: senderAddress,
-        suggestedParams: params,
-        onComplete: algosdk.OnApplicationComplete.NoOpOC,
-        approvalProgram: compiledApprovalProgram,
-        clearProgram: compiledClearProgram,
-        numLocalInts: numLocalInts,
-        numLocalByteSlices: numLocalBytes,
-        numGlobalInts: numGlobalInts,
-        numGlobalByteSlices: numGlobalBytes,
-        note: note,
-        appArgs: appArgs
-    });
-
-    // Get transaction ID
-    let txId = txn.txID().toString();
-
-    // Sign & submit the transaction
-    let signedTxn = await myAlgoConnect.signTransaction(txn.toByte());
-    console.log("Signed transaction with txID: %s", txId);
-    await algodClient.sendRawTransaction(signedTxn.blob).do();
-
-    // Wait for transaction to be confirmed
-    let confirmedTxn = await algosdk.waitForConfirmation(algodClient, txId, 4);
-
-    // Get the completed Transaction
-    console.log("Transaction " + txId + " confirmed in round " + confirmedTxn["confirmed-round"]);
-
-    // Get created application id and notify about completion
-    let transactionResponse = await algodClient.pendingTransactionInformation(txId).do();
-    let appId = transactionResponse['application-index'];
-    console.log("Created new app-id: ", appId);
-    return appId;
 }
 
 // Compile smart contract in .teal format to program
@@ -143,8 +80,7 @@ export const createProductAction = async (senderAddress, product) => {
     await algodClient.sendRawTransaction(signedTxn.blob).do();
 
     // Wait for transaction to be confirmed
-    let staking fee is required, returned upon successful completion of the program. Please check the schedule below for availability. We look forward to meeting you! 😄
-    confirmedTxn = await algosdk.waitForConfirmation(algodClient, txId, 4);
+    let confirmedTxn = await algosdk.waitForConfirmation(algodClient, txId, 4);
 
     // Get the completed Transaction
     console.log("Transaction " + txId + " confirmed in round " + confirmedTxn["confirmed-round"]);
